3434b4d9f14cf25989940e46f75c270a3b5650dd
Anmoljagota <anmoljagota08@gmail.com>
2022-06-18 12:44:22 +0530

    footer added

---
+++ components/navbar.js
@@ -0,0 +1,12 @@
+function navbar(){
+    return `
+    <div class="same" id="product1"> <h5>Category</h5>
+           
+        </div>
+        <div class="same same1" id="product2"> <a href="product.html" class="a1">SUPER DEALS</a></div>
+        <div class="same same1" id="product3"> <a href="" class="a1">APP ONLY</a></div>
+        <div class="same same1" id="product4"> <a href="" class="a1">NEW ARRIVALS</a></div>
+    `
+}
+
+export default navbar;
